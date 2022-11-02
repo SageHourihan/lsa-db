d7d017413c5a2f34c23ca3b6dd165bb43cd81ac8
Sage Hourihan <samiho97@gmail.com>
2022-11-02 13:59:52 -0400

    added rest of fields to the controller to post to db

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -14,8 +14,27 @@ exports.create = (req, res) => {
         address: req.body.address,
         dues: req.body.dues,
         status: req.body.status,
-        water: req.body.water
-        // TODO: Add area and cardNumber
+        water: req.body.water,
+        area: req.body.area,
+        cardNumber: req.body.cardNumber,
+        cardEnabled: req.body.cardEnabled,
+        agreement: req.body.agreement,
+        registration: req.body.registration,
+        wc1: req.body.wc1,
+        mc1Num: req.body.mcNum1,
+        mc1Color: req.body.mc1Color,
+        wc2: req.body.wc1,
+        mc2Num: req.body.mcNum1,
+        mc2Color: req.body.mc1Color,
+        wc3: req.body.wc1,
+        mc3Num: req.body.mcNum1,
+        mc3Color: req.body.mc1Color,
+        wc4: req.body.wc1,
+        mc4Num: req.body.mcNum1,
+        mc4Color: req.body.mc1Color,
+        wc5: req.body.wc1,
+        mc5Num: req.body.mcNum1,
+        mc5Color: req.body.mc1Color
     })
 
     // save user in the database
