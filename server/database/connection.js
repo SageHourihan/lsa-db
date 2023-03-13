8e9a8a181f36a791cd6fecd21886b3273f4b9b8e
Sage Hourihan <samiho97@gmail.com>
2023-03-13 16:55:40 -0400

    made updates to db schema to store data in array

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_PROD, {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
--- server/model/model.js
+++ server/model/model.js
@@ -1,40 +1,50 @@
 const mongoose = require('mongoose');
 
+// var schema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     address: {
+//         type: String,
+//         required: true,
+//     },
+//     dues: String,
+//     status: String,
+//     water: String,
+//     area: Number,
+//     cardNumber: Number,
+//     cardEnabled: String,
+//     agreement: String,
+//     registration: String,
+//     wc1: String,
+//     mc1Num: String,
+//     mc1Color: String,
+//     wc2: String,
+//     mc2Num: String,
+//     mc2Color: String,
+//     wc3: String,
+//     mc3Num: String,
+//     mc3Color: String,
+//     wc4: String,
+//     mc4Num: String,
+//     mc4Color: String,
+//     wc5: String,
+//     mc5Num: String,
+//     mc5Color: String,
+//     __v: { type: Number, select: false }
+// })
+
 var schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    dues: String,
-    status: String,
-    water: String,
-    area: Number,
-    cardNumber: Number,
-    cardEnabled: String,
-    agreement: String,
-    registration: String,
-    wc1: String,
-    mc1Num: String,
-    mc1Color: String,
-    wc2: String,
-    mc2Num: String,
-    mc2Color: String,
-    wc3: String,
-    mc3Num: String,
-    mc3Color: String,
-    wc4: String,
-    mc4Num: String,
-    mc4Color: String,
-    wc5: String,
-    mc5Num: String,
-    mc5Color: String,
-    __v: { type: Number, select: false }
+    name: String,
+    address: String,
+    boats: [{
+        mc: String,
+        make: String,
+        color: String
+    }]
 })
 
-const Userdb = mongoose.model('userdb', schema);
+const Userdb = mongoose.model('userdb', schema, 'test');
 
 module.exports = Userdb;
